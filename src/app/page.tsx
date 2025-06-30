2b20be19a39a52566a2a2e1b89a80260a33d9389
Seng Chanthea <sengchantheaz123@gmail.com>
2025-06-30 20:00:45 +0700

    config error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1 className="text-xl md:text-2xl lg:text-4xl font-bold text-center text-gray-800 px-4">
         <span className="mb-2 md:mb-4 inline-block">Welcome to the void.</span>
         <br className="hidden md:block" />
-        Now prove you're worthy — guess the route.
+        Now prove you&apos;re worthy — guess the route.
       </h1>
     </div>
   );
