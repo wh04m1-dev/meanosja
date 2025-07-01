b1086d8c144e24cda465b93b41422331944e00d1
Seng Chanthea <sengchantheaz123@gmail.com>
2025-07-01 08:21:40 +0700

    add profile

---
+++ src/app/otmeante/page.tsx
@@ -0,0 +1,11 @@
+export default function otmeante() {
+    return (
+        <div>
+            <main>
+                <h1>
+                    Welcome to the Kubernetes Contributor Community!
+                </h1>
+            </main>
+        </div>
+    );
+}
